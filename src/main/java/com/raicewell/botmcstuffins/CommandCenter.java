31a6ad443ce8d021447fbf90183bce9c17df5933
Raice <Raice@Raice.fios-router.home>
2016-09-01 17:08:32 -0400

    Added contextLoader object to get sound files from context.

--- src/main/java/com/raicewell/botmcstuffins/CommandCenter.java
+++ src/main/java/com/raicewell/botmcstuffins/CommandCenter.java
@@ -86,7 +86,7 @@ public class CommandCenter {
         try{
             AudioPlayer player = AudioPlayer.getAudioPlayerForGuild(channel.getGuild());
             channel.join();
-            resourcePath = Thread.currentThread().getContextClassLoader().getResource("/" + filename + ".wav").getFile();
+            resourcePath = CommandCenter.class.getClassLoader().getResource(filename + ".wav").getFile();
             //resourcePath = "/home/theraice/Programs/McStuffinsBot/McStuffinsBot/target/classes/" + filename + ".wav" ;
             File getFile = new File(resourcePath);            
             player.queue(getFile);
